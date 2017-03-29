9d927bf6dd24c18c98bf75d476553bd86566a78e
Jari van Dam <jari@jarivandam.nl>
2017-03-29 12:41:47 +0200

    stop on object

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #pragma config(Sensor, S3, lightSensor, sensorLightActive)
 #pragma config(Sensor, S2, sonarSensor, sensorSONAR)
 #pragma config(Sensor, S1, colorPort,  sensorColorNxtFULL)
-//#include "steering.h"
+#include "steering.h"
 //#include "monitoring.h"
 //#include "commands.h"
 task main()
@@ -24,43 +24,55 @@ task main()
 	*/
 	int direction = 0;
 
+	// max speed is 50
+	int speed = 10;
 	while (status >= 0){
-			displayTextLine(2, "  While");
-		//if(status == 1){
-		//	if(check_obstacle()){
-		//			brake(10);
-		//	}
-		//	else if(check_crossroad()){
-		//			brake(10);
-		//	}
-		//	else if (check_track()){
-
-		//	}
+				// 0 is darker 100 is lighter
 		int right_sensor = SensorValue(lightSensor);
 		// 1 = black 6 = white
 		int left_sensor = SensorValue[colorPort];
+
 		nxtDisplayTextLine(4, "Color: %d",left_sensor);
 		nxtDisplayTextLine(5, "Light: %d",right_sensor);
-		if(right_sensor < 45 && left_sensor == 1){
-			displayTextLine(3, " Forward");
-			motor[motorA] = 20;
-			motor[motorC] = 20;
+
+		int distance = SensorValue[sonarSensor];
+		nxtDisplayTextLine(2, "Distance: %d",distance);
+		if ( distance < 15 ){
+			brake(10);
 		}
-		 //Left turn
+
+		// Forward
+		if(right_sensor > 45 && left_sensor == 6){
+			displayTextLine(3, " Forward");
+			status = 1;
+			drive(speed);
+		}
+
+		// Crossroad
+		else if (right_sensor < 45 && left_sensor == 1){
+
+			if (status != 3){
+				drive(-5);
+				wait1Msec(250);
+				brake(10);
+			}
+			status = 3;
+		}
+
+		//Left turn
 		else if (right_sensor > 45 && left_sensor == 1 ){
 			displayTextLine(3, " Left");
 			status = 2;
 			direction = 1;
-			motor[motorA] = 5;
-			motor[motorC] = 20;
+			turn(-12,speed);
 		}
-		//// Right turn
+
+		// Right turn
 		else if (right_sensor < 45 && left_sensor == 6){
 			displayTextLine(3, " Right");
 			status = 2;
-			direction = 1;
-			motor[motorA] = 20;
-			motor[motorC] = 5;
+			direction = 2;
+			turn(12,speed);
 		}
 	}
 }
--- steering.h
+++ steering.h
@@ -1,5 +1,33 @@
+/*! \brief Give the robot a certain speed
+ *
+ *	\param[in] int speed, the speed of the robot
+ */
 void drive(int speed){
-	motor[motorB] = speed;
+	motor[motorA] = speed;
+	motor[motorC] = speed;
 }
-void turn(int degrees);
-void brake(int time_to_stop);
+/*! \brief Turn the robot left or right depending on the degrees given
+ *
+ *	\param[in] int degrees, positve degrees will result in a right turn, negative degrees will result in a left turn
+ * 	\param[in] int speed, speed of the robot
+ */
+void turn(int degrees, int speed){
+	// right turn
+	if(degrees >= 0){
+			motor[motorA] = (speed * 1);
+			motor[motorC] = -(speed * 1.75);
+		}
+		//left turn
+	else if (degrees < 0){
+			motor[motorA] = -(speed * 1.75);
+			motor[motorC] = (speed * 1);
+	}
+}
+/*! \brief Stop the robot in a certain time
+ *
+ *	\param[in] int time_to_stop, the time in wich the robot must be stading still
+ */
+void brake(int time_to_stop){
+	motor[motorA] = 0;
+	motor[motorC] = 0;
+}

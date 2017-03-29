801c8c8b15a3fb51c0026c05d30d32c634af2f6f
Vince van Noort <vince.vannoort@student.hu.nl>
2017-03-29 07:11:01 -0700

    Cleaned up code

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 //#include "commands.h"
 task main()
 {
-	wait10Msec(50);
 	/*
 	*		Status describes the state the robot is currently in.
 	*   0 = Robot does nothing
@@ -16,6 +15,7 @@ task main()
 	*  	4 = Obstacle detected
 	*/
 	int status = 0;
+
 	/*
 	* 	The direction the robot is turning to
 	* 	0 = Straight
@@ -23,33 +23,35 @@ task main()
 	*		2 = Right
 	*/
 	int direction = 0;
+	int speed = 4;
 
-	// max speed is 50
-	int speed = 12;
+
 	while (status >= 0){
-		// white is 60 black is 30
-		int right_sensor = SensorValue(lightSensor);
-		// white is 47 black is 20
-		int left_sensor = SensorValue[colorPort];
+		int right_sensor = SensorValue(lightSensor); // white is 60 black is 30
+		int left_sensor = SensorValue[colorPort]; // white is 47 black is 20
+		int distance = SensorValue[sonarSensor];
 
+		// Temporary debugging values
 		nxtDisplayTextLine(4, "Left: %d",left_sensor);
 		nxtDisplayTextLine(5, "Right: %d",right_sensor);
-
-		int distance = SensorValue[sonarSensor];
 		nxtDisplayTextLine(2, "Distance: %d",distance);
+
+		// Obstacle detected
 		if ( distance < 15 ){
 			brake(10);
+			status = 4;
 		}
 
-
-		// Crossroad
-		if (right_sensor < 35 && left_sensor < 25){
+		// Crossroad detected
+		else if (right_sensor < 35 && left_sensor < 25){
 			brake(10);
 			status = 3;
 		}
-		else if ( status != 3){
-			motor[motorA] = (((left_sensor - 20) / 3) + ((left_sensor - 20) / 30)) * 4;
-			motor[motorC] = (((right_sensor - 30)/3)) * 4;
+
+		// Following line
+		else if ( status != 3 && status != 4){
+			motor[motorA] = (((left_sensor - 20) / 3) + ((left_sensor - 20) / 30)) * speed;
+			motor[motorC] = (((right_sensor - 30)/3)) * speed;
 		}
 	}
 }

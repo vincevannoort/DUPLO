f9e3a2f7bb334a5436bcae9ed1452a9b60c5eefe
vasilishomegas <G!tHub7>
2017-04-03 14:54:38 +0200

    Gradual braking, avoid_obstacle function

--- steering.h
+++ steering.h
@@ -16,17 +16,30 @@ void turn(int motor_left, int motor_right, int speed, int turn_time) {
 *
 *	\param[in] int time_to_stop, the time in which the robot must be standing still
 */
-void brake(int time_to_stop, int *speed, int *left_speed, int *right_speed, int *status){
-
-	for(int i = time_to_stop-1; i > 0; --i){
-		float f = (float)i/(float)time_to_stop;
-		*left_speed *= f;
-		*right_speed *= f;
-		motor[motorA] = *left_speed;
-		motor[motorC] = *right_speed;
-		wait1Msec(50);
+void brake(int time_to_stop, int *speed, int *left_speed, int *right_speed){
+	if (time_to_stop != 0) {
+		for(int i = time_to_stop-1; i > 0; --i){
+			if (*left_speed <= 0 && *right_speed <= 0){
+				break;
+			}
+			float f = (float)i/(float)time_to_stop;
+			*left_speed *= f;
+			*right_speed *= f;
+			motor[motorA] = *left_speed;
+			motor[motorC] = *right_speed;
+			wait1Msec(50);
+		}
+	} else {
+		motor[motorA] = 0;
+		motor[motorC] = 0;
 	}
 	clearSounds();
-	*status = 5;
+	status = 5;
 	*speed = 5;
+}
+
+void avoid_obstacle(int *speed, int *left_speed, int *right_speed, int turn_time){
+	brake(100, speed, left_speed, right_speed);
+	turn(5, -5, *speed, turn_time*2);
+	status = 1;
 }
